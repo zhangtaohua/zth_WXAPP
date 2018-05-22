0f45768cfff2c7179f4866a4a99b2f1fdc34c05d
zhangtaohua <zthvivid@163.com>
2018-05-22 18:01:42 +0800

    9.1 user permrs

--- src/mixins/replyMixin.js
+++ src/mixins/replyMixin.js
@@ -71,7 +71,9 @@ export default class ReplyMixin extends wepy.mixin {
       return false
     }
 
-    return (reply.user_id === user.id)
+    // return (reply.user_id === user.id)
+    // 用户未回复发布者 或 有管理内容权限
+    return (reply.user_id === user.id) || this.$parent.can('manage_contents')
   }
 
   async onPullDownRefresh() {
