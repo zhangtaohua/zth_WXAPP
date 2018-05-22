92adec08d5b27c04acc8c9bb21ef743458e620de
zhangtaohua <zthvivid@163.com>
2018-05-22 14:28:55 +0800

    add moment.js  use for the time show

---
+++ src/utils/utils.js
@@ -0,0 +1,11 @@
+mport moment from 'moment'
+import 'moment/locale/zh-cn'
+
+const diffForHumans = (date, format='YYYYMMDD H:mm:ss') => {
+  moment.locale('zh-cn')
+  return moment(date, format).fromNow()
+}
+
+export default {
+  diffForHumans
+}
